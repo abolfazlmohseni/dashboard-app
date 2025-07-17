e9252395e6e32320ef5224fad1eaa4b65fc4ab34
Abolfazl Mohseni <131040476+abolfazlmohseni@users.noreply.github.com>
2025-07-17 19:46:11 +0330

    config and final login page

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import Login from "./page/login&more/login/login";
+
+const root = ReactDom.createRoot(document.querySelector("#root"));
+
+root.render(<Login />);
